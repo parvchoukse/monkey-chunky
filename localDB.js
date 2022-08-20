3de664bf4f7c75605ac8a8d56266ff10b42630d0
parvchoukse <87237502+parvchoukse@users.noreply.github.com>
2022-08-20 20:14:26 +0530

    Add files via upload

---
+++ localDB.js
@@ -0,0 +1,336 @@
+const db={
+	"important": {
+		"chunks": ["i", "m", "p", "o", "r", "t", "a", "n", "t"],
+		"phones": ["IH", "M", "P", "AO", "R", "T", "AH", "N", "T"]
+	},
+	"field": {
+		"chunks": ["f", "ie", "l", "d"],
+		"phones": ["F", "IY", "L", "D"]
+	},
+"parv":{
+  "chunks":["pa","r","v"],
+  "phones":["pa","r","v"]
+},
+
+	"few": {
+		"chunks": ["f", "e", "w"],
+		"phones": ["F", "Y", "UW"]
+	},
+	"east": {
+		"chunks": ["ea", "s", "t"],
+		"phones": ["IY", "S", "T"]
+	},
+	"paper": {
+		"chunks": ["p", "a", "p", "er"],
+		"phones": ["P", "EY", "P", "ER"]
+	},
+	"single": {
+		"chunks": ["s", "i", "n", "g", "$", "le"],
+		"phones": ["S", "IH", "NG", "G", "AH", "L"]
+	},
+	"ii": {
+		"chunks": [],
+		"phones": []
+	},
+	"age": {
+		"chunks": ["a", "ge"],
+		"phones": ["EY", "JH"]
+	},
+	"activities": {
+		"chunks": ["a", "c", "t", "i", "v", "i", "t", "i", "es"],
+		"phones": ["AE", "K", "T", "IH", "V", "IH", "T", "IY", "Z"]
+	},
+	"club": {
+		"chunks": ["c", "l", "u", "b"],
+		"phones": ["K", "L", "AH", "B"]
+	},
+	"example": {
+		"chunks": ["e", "x", "$", "a", "m", "p", "$", "le"],
+		"phones": ["IH", "G", "Z", "AE", "M", "P", "AH", "L"]
+	},
+	"girls": {
+		"chunks": ["g", "ir", "l", "s"],
+		"phones": ["G", "ER", "L", "Z"]
+	},
+	"additional": {
+		"chunks": ["a", "dd", "i", "t", "io", "n", "a", "l"],
+		"phones": ["AH", "D", "IH", "SH", "AH", "N", "AH", "L"]
+	},
+	"password": {
+		"chunks": ["p", "a", "ss", "w", "or", "d"],
+		"phones": ["P", "AE", "S", "W", "ER", "D"]
+	},
+	"z": {
+		"chunks": ["z", "$"],
+		"phones": ["Z", "IY"]
+	},
+	"latest": {
+		"chunks": ["l", "a", "t", "e", "s", "t"],
+		"phones": ["L", "EY", "T", "AH", "S", "T"]
+	},
+	"something": {
+		"chunks": ["s", "o", "me", "th", "i", "ng"],
+		"phones": ["S", "AH", "M", "TH", "IH", "NG"]
+	},
+	"road": {
+		"chunks": ["r", "oa", "d"],
+		"phones": ["R", "OW", "D"]
+	},
+	"gift": {
+		"chunks": ["g", "i", "f", "t"],
+		"phones": ["G", "IH", "F", "T"]
+	},
+	"question": {
+		"chunks": ["q", "u", "e", "s", "t", "io", "n"],
+		"phones": ["K", "W", "EH", "S", "CH", "AH", "N"]
+	},
+	"changes": {
+		"chunks": ["ch", "a", "n", "g", "e", "s"],
+		"phones": ["CH", "EY", "N", "JH", "AH", "Z"]
+	},
+	"night": {
+		"chunks": ["n", "igh", "t"],
+		"phones": ["N", "AY", "T"]
+	},
+	"ca": {
+		"chunks": ["c", "a"],
+		"phones": ["K", "AH"]
+	},
+	"hard": {
+		"chunks": ["h", "a", "r", "d"],
+		"phones": ["HH", "AA", "R", "D"]
+	},
+	"texas": {
+		"chunks": ["t", "e", "x", "$", "a", "s"],
+		"phones": ["T", "EH", "K", "S", "AH", "S"]
+	},
+	"oct": {
+		"chunks": ["o", "c", "t"],
+		"phones": ["AO", "K", "T"]
+	},
+	"pay": {
+		"chunks": ["p", "ay"],
+		"phones": ["P", "EY"]
+	},
+	"four": {
+		"chunks": ["f", "ou", "r"],
+		"phones": ["F", "AO", "R"]
+	},
+	"poker": {
+		"chunks": ["p", "o", "k", "er"],
+		"phones": ["P", "OW", "K", "ER"]
+	},
+	"status": {
+		"chunks": ["s", "t", "a", "t", "e", "s"],
+		"phones": ["S", "T", "AE", "T", "AH", "S"]
+	},
+	"browse": {
+		"chunks": ["b", "r", "ow", "se"],
+		"phones": ["B", "R", "AW", "Z"]
+	},
+	"issue": {
+		"chunks": ["i", "ss", "ue"],
+		"phones": ["IH", "SH", "UW"]
+	},
+	"range": {
+		"chunks": ["r", "a", "n", "ge"],
+		"phones": ["R", "EY", "N", "JH"]
+	},
+	"building": {
+		"chunks": ["b", "ui", "l", "d", "i", "ng"],
+		"phones": ["B", "IH", "L", "D", "IH", "NG"]
+	},
+	"seller": {
+		"chunks": ["s", "e", "ll", "er"],
+		"phones": ["S", "EH", "L", "ER"]
+	},
+	"court": {
+		"chunks": ["c", "ou", "r", "t"],
+		"phones": ["K", "AO", "R", "T"]
+	},
+	"february": {
+		"chunks": ["f", "e", "b", "r", "$", "u", "a", "r", "y"],
+		"phones": ["F", "EH", "B", "Y", "AH", "W", "EH", "R", "IY"]
+	},
+	"always": {
+		"chunks": ["a", "l", "w", "ay", "s"],
+		"phones": ["AO", "L", "W", "EY", "Z"]
+	},
+	"result": {
+		"chunks": ["r", "e", "s", "u", "l", "t"],
+		"phones": ["R", "IH", "Z", "AH", "L", "T"]
+	},
+	"audio": {
+		"chunks": ["au", "d", "i", "o"],
+		"phones": ["AA", "D", "IY", "OW"]
+	},
+	"light": {
+		"chunks": ["l", "igh", "t"],
+		"phones": ["L", "AY", "T"]
+	},
+	"write": {
+		"chunks": ["wr", "i", "te"],
+		"phones": ["R", "AY", "T"]
+	},
+	"war": {
+		"chunks": ["w", "a", "r"],
+		"phones": ["W", "AO", "R"]
+	},
+	"nov": {
+		"chunks": ["n", "o", "v"],
+		"phones": ["N", "OW", "V"]
+	},
+	"offer": {
+		"chunks": ["o", "ff", "er"],
+		"phones": ["AO", "F", "ER"]
+	},
+	"blue": {
+		"chunks": ["b", "l", "ue"],
+		"phones": ["B", "L", "UW"]
+	},
+	"groups": {
+		"chunks": ["g", "r", "ou", "p", "s"],
+		"phones": ["G", "R", "UW", "P", "S"]
+	},
+	"al": {
+		"chunks": ["a", "l"],
+		"phones": ["AE", "L"]
+	},
+	"easy": {
+		"chunks": ["ea", "s", "y"],
+		"phones": ["IY", "Z", "IY"]
+	},
+	"given": {
+		"chunks": ["g", "i", "v", "e", "n"],
+		"phones": ["G", "IH", "V", "AH", "N"]
+	},
+	"files": {
+		"chunks": ["f", "i", "l", "es"],
+		"phones": ["F", "AY", "L", "Z"]
+	},
+	"event": {
+		"chunks": ["e", "v", "e", "n", "t"],
+		"phones": ["IH", "V", "EH", "N", "T"]
+	},
+	"release": {
+		"chunks": ["r", "e", "l", "ea", "se"],
+		"phones": ["R", "IY", "L", "IY", "S"]
+	},
+	"analysis": {
+		"chunks": ["a", "n", "a", "l", "y", "s", "i", "s"],
+		"phones": ["AE", "N", "AE", "L", "IH", "S", "IH", "S"]
+	},
+	"request": {
+		"chunks": ["r", "e", "q", "u", "e", "s", "t"],
+		"phones": ["R", "IH", "K", "W", "EH", "S", "T"]
+	},
+	"fax": {
+		"chunks": ["f", "a", "x", "$"],
+		"phones": ["F", "AE", "K", "S"]
+	},
+	"china": {
+		"chunks": ["ch", "i", "n", "a"],
+		"phones": ["CH", "AY", "N", "AH"]
+	},
+	"making": {
+		"chunks": ["m", "a", "k", "i", "ng"],
+		"phones": ["M", "EY", "K", "IH", "NG"]
+	},
+	"picture": {
+		"chunks": ["p", "i", "c", "t", "ure"],
+		"phones": ["P", "IH", "K", "CH", "ER"]
+	},
+	"needs": {
+		"chunks": ["n", "ee", "d", "s"],
+		"phones": ["N", "IY", "D", "Z"]
+	},
+	"possible": {
+		"chunks": ["p", "o", "ss", "i", "b", "$", "le"],
+		"phones": ["P", "AA", "S", "AH", "B", "AH", "L"]
+	},
+	"might": {
+		"chunks": ["m", "igh", "t"],
+		"phones": ["M", "AY", "T"]
+	},
+	"professional": {
+		"chunks": ["p", "r", "o", "ff", "e", "s", "io", "n", "a", "l"],
+		"phones": ["P", "R", "AH", "F", "EH", "SH", "AH", "N", "AH", "L"]
+	},
+	"yet": {
+		"chunks": ["y", "e", "t"],
+		"phones": ["Y", "EH", "T"]
+	},
+	"month": {
+		"chunks": ["m", "o", "n", "th"],
+		"phones": ["M", "AH", "N", "TH"]
+	},
+	"major": {
+		"chunks": ["m", "a", "j", "or"],
+		"phones": ["M", "EY", "JH", "ER"]
+	},
+	"star": {
+		"chunks": ["s", "t", "a", "r"],
+		"phones": ["S", "T", "AA", "R"]
+	},
+	"areas": {
+		"chunks": ["a", "r", "e", "a", "s"],
+		"phones": ["EH", "R", "IY", "AH", "Z"]
+	},
+	"future": {
+		"chunks": ["f", "u", "$", "t", "ure"],
+		"phones": ["F", "Y", "UW", "CH", "ER"]
+	},
+	"space": {
+		"chunks": ["s", "p", "a", "ce"],
+		"phones": ["S", "P", "EY", "S"]
+	},
+	"committee": {
+		"chunks": ["c", "o", "mm", "i", "t", "ee"],
+		"phones": ["K", "AH", "M", "IH", "T", "IY"]
+	},
+	"hand": {
+		"chunks": ["h", "a", "n", "d"],
+		"phones": ["HH", "AE", "N", "D"]
+	},
+	"sun": {
+		"chunks": ["s", "u", "n"],
+		"phones": ["S", "AH", "N"]
+	},
+	"cards": {
+		"chunks": ["c", "a", "r", "d", "s"],
+		"phones": ["K", "AA", "R", "D", "Z"]
+	},
+	"problems": {
+		"chunks": ["p", "r", "o", "b", "l", "e", "m", "s"],
+		"phones": ["P", "R", "AA", "B", "L", "AH", "M", "Z"]
+	},
+	"london": {
+		"chunks": ["l", "o", "n", "d", "o", "n"],
+		"phones": ["L", "AH", "N", "D", "AH", "N"]
+	},
+	"washington": {
+		"chunks": ["w", "a", "sh", "i", "ng", "t", "o", "n"],
+		"phones": ["W", "AA", "SH", "IH", "NG", "T", "AH", "N"]
+	},
+	"meeting": {
+		"chunks": ["m", "ee", "t", "i", "ng"],
+		"phones": ["M", "IY", "T", "IH", "NG"]
+	},
+	"rss": {
+		"chunks": [],
+		"phones": []
+	},
+	"become": {
+		"chunks": ["b", "e", "c", "o", "me"],
+		"phones": ["B", "IH", "K", "AH", "M"]
+	},
+	"interest": {
+		"chunks": ["i", "n", "t", "e", "r", "e", "s", "t"],
+		"phones": ["IH", "N", "T", "AH", "R", "EH", "S", "T"]
+	},
+	"id": {
+		"chunks": ["i", "d"],
+		"phones": ["IH", "D"]
+	},
+	}
+	export default db;
